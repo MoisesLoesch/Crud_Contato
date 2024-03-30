8c4e315a4f7817f663b6c87629aa48794da96904
MoisesLoesch <61070455+MoisesLoesch@users.noreply.github.com>
2024-03-30 15:44:07 -0300

    Ajustes na autenticação e páginas

--- Crud_Contato/Controllers/ContatosController.cs
+++ Crud_Contato/Controllers/ContatosController.cs
@@ -25,6 +25,10 @@ namespace Crud_Contato.Controllers
         {
             return View(await _context.Contatos.ToListAsync());
         }
+        public async Task<IActionResult> IndexConvidado()
+        {
+            return View(await _context.Contatos.ToListAsync());
+        }        
 
         // GET: Contatos/Details/5
         public async Task<IActionResult> Details(int? id)
--- Crud_Contato/Controllers/HomeController.cs
+++ Crud_Contato/Controllers/HomeController.cs
@@ -1,24 +1,44 @@
 ﻿using Crud_Contato.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Crud_Contato.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BancoDeDados _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BancoDeDados context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(string username, string password)
+        {
+            // Verificar se o usuário e a senha correspondem aos dados no banco de dados
+            var user = _context.AdminUser.FirstOrDefault(u => u.Login == username && u.Senha == password);
+
+            if (user != null)
+                return RedirectToAction("Index", "Contatos");
+            else
+            {
+                // Usuário não autenticado, retornar uma mensagem de erro
+                ViewBag.ErrorMessage = "Usuário ou senha inválidos.";
+                return RedirectToAction("AcessoNegado", "Home");
+            }
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult AcessoNegado()
         {
             return View();
         }
