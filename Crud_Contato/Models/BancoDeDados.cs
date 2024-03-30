d5c1a81a5c462cd92d2b40878a38b4618cdbad64
MoisesLoesch <61070455+MoisesLoesch@users.noreply.github.com>
2024-03-30 12:31:10 -0300

    Adicionar arquivos de projeto.

---
+++ Crud_Contato/Models/BancoDeDados.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Crud_Contato.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public BancoDeDados(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) 
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+            var conexao = _configuration["ConnectionStrings:MariaDB"].ToString();
+            builder.UseMySql(connectionString: conexao, serverVersion: serverVersion);
+        }
+    }
+}
---
+++ Crud_Contato/Models/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace Crud_Contato.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telfone { get; set; }
+        public string Email { get; set; }
+    }
+}
