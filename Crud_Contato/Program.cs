2ee8c6c968d9a0d89cbb8d09f86409d299eb3f5a
MoisesLoesch <61070455+MoisesLoesch@users.noreply.github.com>
2024-04-09 08:28:02 -0300

    Alterações na program.cs para inicializar o banco de dados caso ele não exista

--- Crud_Contato/Program.cs
+++ Crud_Contato/Program.cs
@@ -1,9 +1,13 @@
+using Crud_Contato.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +17,20 @@ namespace Crud_Contato
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var app = CreateHostBuilder(args).Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetService<BancoDeDados>();
+
+                if (dbContext != null && dbContext.Database.GetDbConnection().State != ConnectionState.Open)
+                {
+                    dbContext.Database.OpenConnection();
+                    dbContext.Database.EnsureCreated();
+                }
+            }
+            app.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
